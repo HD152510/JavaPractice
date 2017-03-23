1d4a324e3af9acf039baee557caab89e802d11ca
SngsuNoh <s000715@naver.com>
2017-03-23 09:38:54 +0900

    +Car, +CarTest

---
+++ src/org/dimigo/basic/Car.java
@@ -0,0 +1,45 @@
+package org.dimigo.basic;
+
+/**
+ * Created by seungsunoh on 2017. 3. 23..
+ */
+public class Car {
+        private String company="현대자동차";
+        private String model="제네시스";
+        private String color="검정색";
+        private int maxSpeed=225;
+        private int price=50000000;
+
+        public String getCompany(){
+            return company;
+        }
+        public String getModel(){
+            return model;
+        }
+        public String getColor(){
+            return color;
+        }
+        public int getMaxSpeed(){
+            return maxSpeed;
+        }
+        public int getPrice(){
+            return price;
+        }
+
+        public void setCompany(String newCompany){
+            company=newCompany;
+        }
+        public void setModel(String newModel){
+            model=newModel;
+        }
+        public void setColor(String newColor){
+            color=newColor;
+        }
+        public void setMaxSpeed(int newMaxSpeed){
+            maxSpeed=newMaxSpeed;
+        }
+        public void setPrice(int newPrice){
+            price=newPrice;
+        }
+
+}
