ba7e8c71e47f172755a96b23fc6d5ef453772264
SngsuNoh <s000715@naver.com>
2017-05-24 15:42:00 +0900

    IdolGroup

---
+++ src/org/dimigo/abstractclass/Action.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ *  org.dimigo.abstractclass
+ *     |_ Action
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 18.
+ * </pre>
+ *
+ * @author : ji548
+ * @version : 1.0
+ */
+public abstract class Action {
+	public Action() {
+	}
+	
+	public abstract void execute();
+}
---
+++ src/org/dimigo/abstractclass/UpdateAction.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ *  org.dimigo.abstractclass
+ *     |_ InsertAction
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 18.
+ * </pre>
+ *
+ * @author : ji548
+ * @version : 1.0
+ */
+public class UpdateAction extends Action {
+	public void execute() {
+		System.out.println("데이터를 수정합니다!!");
+	}
+}
